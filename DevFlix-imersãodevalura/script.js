e3bd1f20c580e223ccb897488edbe3470c19b974
Carlos Augusto Goncalves Bezerra <92475408+carlossbezerra@users.noreply.github.com>
2022-03-27 18:56:59 -0300

    Add files via upload

---
+++ DevFlix-imersãodevalura/script.js
@@ -0,0 +1,75 @@
+var Ficção = "Ficção";
+document.write("<h2>" + Ficção + "</h2>");
+
+var listaFicção = [
+  "https://cinearaujo.com.br/filmes/filme3085_cp.png",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6OhYiPGWqEmFRsnRWR2f39cb-hliN442s9MSW7zqHXxHxlVqy",
+  "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTiLxkwoAW7Xp_tB4Pgb5A0oW8xdBsRoXoKGi9TbaYcGRGlV5IZ",
+  "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRhr-vDtOk3JET2i2KxRBIOawqFzmdUxcF07yLWQnKswZWM0eMp",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7FssHe_KH5DbZmja2492HtY872M29XvgcNkTQq0yQHNTVhwTS",
+  "https://upload.wikimedia.org/wikipedia/pt/thumb/0/07/Guardians_of_the_galaxy_vol_two_poster.jpg/250px-Guardians_of_the_galaxy_vol_two_poster.jpg",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRiPBU3UIQYuDqJWK8g4Gc0fAMptpQdWJY25ZBtc0GnjKMWGH5",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROOX3m3tnQ1juDWnxmPq0FMbtYV7HG2Swj5xjozk_9HNiWgEWn",
+  "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQV2ckPgmX4FWU2JrydWDrqJMk8iKtKKirwXyVFSldWSdDjcvHS",
+  "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTfW2FABJLmFJ2-SuTZVQzRhRa2TIWIb1KtJsOYYLLhVffQ084U",
+  "https://miro.medium.com/max/580/1*fWnGk6lXKd6a1tN6WmcCRQ.jpeg",
+  "https://upload.wikimedia.org/wikipedia/pt/thumb/1/1b/The_Incredible_Hulk.jpg/250px-The_Incredible_Hulk.jpg",
+  "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQw17kZheMBS37KECDkmOlZ38XLtdmBIsBmjeKxVlhWpzT_tC43"
+  
+];
+
+for (var i = 0; i < listaFicção.length; i++) {
+  document.write("<img src=" + listaFicção[i] + ">");
+}
+
+var Kids = "Kids";
+document.write("<h2>" + Kids + "</h2>");
+
+var listaKids = [
+"https://pics.filmaffinity.com/Las_aventuras_de_Peabody_y_Sherman-177817247-large.jpg",
+"https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRl_zpGOmcZHdbOOjD-p70IrGzdquqh-TobyTrFe7zjAceNgCWU",
+"https://br.web.img3.acsta.net/pictures/21/01/25/22/43/2182853.jpg",
+"https://br.web.img3.acsta.net/pictures/21/06/16/10/12/3948304.jpg",
+"https://cdn.fstatic.com/media/movies/covers/2019/08/hfhgf.jpg",
+"https://br.web.img3.acsta.net/pictures/21/09/10/18/36/3514646.jpg",
+"https://musicart.xboxlive.com/7/94fc5000-0000-0000-0000-000000000002/504/image.jpg?w=1920&h=1080",
+"https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSBrV9YLDvlYv97A-J6LKkYLbLP488uC8OcBJGS4Xm1vjFby3SU",
+"https://br.web.img2.acsta.net/pictures/16/12/21/17/34/290931.jpg",
+"https://images-na.ssl-images-amazon.com/images/I/51vm5lDN1XL._SX331_BO1,204,203,200_.jpg",
+"https://http2.mlstatic.com/D_NQ_NP_765396-MLB28434689900_102018-O.jpg",
+"https://images-na.ssl-images-amazon.com/images/I/91L80naXOrL.jpg",
+"https://br.web.img2.acsta.net/pictures/21/11/24/09/58/5794996.jpg",
+
+];
+
+for (var i = 0; i < listaKids.length; i++) {
+  document.write("<img src=" + listaKids[i] + ">");
+}
+
+
+var Comédia = "Comédia";
+document.write("<h2>" + Comédia + "</h2>");
+
+var listaComédia = [
+  "https://br.web.img3.acsta.net/c_310_420/pictures/21/10/28/20/55/0671708.jpg",
+  "https://cdn.fstatic.com/media/movies/covers/2018/11/isnt_it_romantic.jpg",
+  "https://br.web.img3.acsta.net/pictures/21/02/26/19/12/3037508.png",
+  "https://br.web.img3.acsta.net/pictures/210/327/21032794_20130828164937825.jpg",
+  "https://br.web.img3.acsta.net/pictures/19/12/12/22/35/3598465.jpg",
+  "https://br.web.img2.acsta.net/pictures/18/05/04/15/03/2133793.jpg",
+  "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSR-vhHxUoTEifB2bTXPjfJKBb13Fr81MdJ1n8Xj0rClaKdoR_-",
+  "https://upload.wikimedia.org/wikipedia/pt/9/9e/Turning_Red_poster.jpg",
+  "https://upload.wikimedia.org/wikipedia/en/thumb/0/0f/The_Ice_Age_Adventures_of_Buck_Wild.jpg/220px-The_Ice_Age_Adventures_of_Buck_Wild.jpg",
+  "https://ingresso-a.akamaihd.net/prd/img/movie/sing-2/26b264cf-5d8b-4325-a8be-658ff475e2ae.jpg",
+  "https://www.cinemundo.pt/wp-content/uploads/2021/08/queenpins_poster_pt_720x1024px_rgb_web.jpg",
+  "https://br.web.img3.acsta.net/pictures/21/04/01/10/40/5886715.jpg",
+  
+];
+
+for (var i = 0; i < listaComédia.length; i++) {
+  document.write("<img src=" + listaComédia[i] + ">");
+}
+
+
+
+
